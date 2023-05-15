ae3f526969bf4d04747243d5cffc41fb41bb9c29
SashGoel <sakshigoel181@gmail.com>
2023-05-15 13:06:26 +0530

    updated portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import weather from "../../Assets/Projects/weatherapp.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import movieverse from "../../Assets/Projects/movieverse-1.png";
 import filesharing from "../../Assets/Projects/filesharing.png";
 import instaclone from "../../Assets/Projects/instaclone.png";
+import portfolio from "../../Assets/Projects/portfolio.png";
 
 function Projects() {
   return (
@@ -17,16 +18,6 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description=""
-              ghLink=""
-              demoLink=""
-            />
-          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
@@ -35,6 +26,29 @@ function Projects() {
               title="Instagram Clone"
               description="This is an Instagram clone app built using the MERN stack (MongoDB, Express.js, React, and Node.js). The app allows users to post photos, follow other users, like and comment on posts, and view their profile and feed."
               ghLink="https://github.com/SashGoel/instagram-clone.git"
+              demoLink="https://instaagramclone.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={movieverse}
+              isBlog={false}
+              title="Movieverse"
+              description="This is a movie app built using ReactJS, Redux, and SCSS. The app utilizes the TMDB API to allow users to search for movies and TV shows, view movie and TV show details, watch trailers, and check ratings."
+              ghLink="https://github.com/SashGoel/movieverse.git"
+              demoLink="https://movieeverse.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={portfolio}
+              isBlog={false}
+              title="Portfolio"
+              description="This React-based personal portfolio website serves as a platform to display my projects, highlight my skills, and offers a means for individuals to contact me."
+              ghLink="https://github.com/SashGoel/my-portfolio.git"
+              demoLink="https://sakshigoel.vercel.app/"
             />
           </Col>
 
